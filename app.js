0053373ea24c736b51bccd49688c0a7b41a03dab
Adekunle <oladitilukuman09@gmail.com>
2026-08-11 22:14:13 +0100

    Apple-Website

---
+++ app.js
@@ -0,0 +1,8 @@
+// alert("Hello, World!");
+
+const navToggle = document.getElementById('navToggle');
+const navLinks = document.getElementById('navLinks');
+
+navToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
